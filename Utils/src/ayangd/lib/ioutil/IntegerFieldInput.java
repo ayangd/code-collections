83a70597a1d3e95835b0b26be95c01fdef612c5f
ayangd <jimmyhendrixx909@gmail.com>
2020-03-23 17:11:28 +0700

    Added Utils and HR Manager Java projects

---
+++ Utils/src/ayangd/lib/ioutil/IntegerFieldInput.java
@@ -0,0 +1,61 @@
+package ayangd.lib.ioutil;
+
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class IntegerFieldInput {
+	private int minimumValue, maximumValue;
+	private String prompt;
+	
+	public IntegerFieldInput() {
+		minimumValue = Integer.MIN_VALUE;
+		maximumValue = Integer.MAX_VALUE;
+		prompt = null;
+	}
+	
+	public IntegerFieldInput setMinimumValue(int min) {
+		minimumValue = min;
+		return this;
+	}
+	
+	public IntegerFieldInput setMaximumValue(int max) {
+		maximumValue = max;
+		return this;
+	}
+	
+	public IntegerFieldInput setPrompt(String prompt) {
+		this.prompt = prompt;
+		return this;
+	}
+	
+	public int runField(Scanner sc) {
+		while (true) {
+			int input = 0;
+			try {
+				if (prompt != null)
+					System.out.printf("%s: ", prompt);
+				
+				input = sc.nextInt();
+				
+				// Check for input criteria
+				if (input >= minimumValue && input <= maximumValue)
+					return input;
+				
+				// Output the appropriate value boundary.
+				if (minimumValue == Integer.MIN_VALUE)
+					System.out.printf("Please input value below %d.\n", maximumValue);
+				else if (maximumValue == Integer.MAX_VALUE)
+					System.out.printf("Please input value above %d.\n", minimumValue);
+				else
+					System.out.printf("Please input value between %d and %d\n", minimumValue, maximumValue);
+				
+			} catch (NumberFormatException e) {
+				System.out.println("Please input a number.");
+			} catch (NoSuchElementException e) {
+				throw new IllegalStateException("Stream exhausted.");
+			} catch (IllegalStateException e) {
+				throw new IllegalStateException("Stream closed.");
+			}
+		}
+	}
+}
